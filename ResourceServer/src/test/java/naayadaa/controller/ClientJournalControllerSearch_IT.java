c1945723f8f2bbba9c95ea23567de67c3b90d3c9
AnastasiiaDepenchuk <anastasiia.sepenchuk@advantio.com>
2018-04-30 09:55:05 +0300

    search tests

---
+++ ResourceServer/src/test/java/naayadaa/controller/ClientJournalControllerSearch_IT.java
@@ -0,0 +1,213 @@
+package naayadaa.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import naayadaa.AbstractIntegrationTest;
+import naayadaa.dto.ClientDTO;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by AnastasiiaDepenchuk on 29-Apr-18.
+ */
+@ActiveProfiles("test")
+@DatabaseSetup(value = {"classpath:data.xml"})
+@ExpectedDatabase(value = "classpath:data.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+@DatabaseTearDown(value = {"classpath:tear_down.xml"})
+public class ClientJournalControllerSearch_IT extends AbstractIntegrationTest {
+
+    @Resource
+    private FilterChainProxy springSecurityFilterChain;
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private Map<Integer, ClientDTO> clientDTOS = new HashMap<>();
+
+    @Before
+    public void setup() throws IOException {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).addFilter(springSecurityFilterChain).build();
+        List<ClientDTO> clientDTOList = objectMapper.readValue(this.getClass().getClassLoader()
+                .getResourceAsStream("clients.json"), new TypeReference<List<ClientDTO>>() {
+        });
+        for (ClientDTO clientDTO : clientDTOList) {
+            clientDTOS.put(clientDTO.getId(), clientDTO);
+        }
+    }
+
+    /*
+   * A not logged-in user trying to access to the endpoint will receive a 401 HTTP error
+   */
+    @Test
+    public void getClients_WithUnauthorisedUser() throws Exception {
+        mockMvc.perform(post("/client-journal-resource/clients/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[{\n" +
+                        "    \"field\": \"firstName\",\n" +
+                        "    \"operation\": \"EQUALITY\",\n" +
+                        "    \"orPredicate\": true,\n" +
+                        "    \"value\": \"Vasya\"\n" +
+                        "  },\n" +
+                        "  {\n" +
+                        "    \"field\": \"firstName\",\n" +
+                        "    \"operation\": \"EQUALITY\",\n" +
+                        "    \"orPredicate\": true,\n" +
+                        "    \"value\": \"Masha\"\n" +
+                        "  }]"))
+                .andExpect(status().is(401));
+    }
+
+
+    //find all clients where names are equals "Vasya" or "Masha"
+    @Test
+    public void find1_Test_OK() throws Exception {
+        String searchRequest ="[{\n" +
+                "    \"field\": \"firstName\",\n" +
+                "    \"operation\": \"EQUALITY\",\n" +
+                "    \"orPredicate\": true,\n" +
+                "    \"value\": \"Vasya\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"field\": \"firstName\",\n" +
+                "    \"operation\": \"EQUALITY\",\n" +
+                "    \"orPredicate\": true,\n" +
+                "    \"value\": \"Masha\"\n" +
+                "  }]";
+        RequestPostProcessor bearerToken = addBearerToken("test");
+
+        MvcResult result = mockMvc.perform(post("/client-journal-resource/clients/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(searchRequest)
+                .with(bearerToken))
+                .andExpect(status().is(200)).andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+        List<ClientDTO> clients = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<ClientDTO>>() {
+        });
+        Assert.assertEquals(2, clients.size());
+        for (ClientDTO client : clients) {
+            Assert.assertEquals(clientDTOS.get(client.getId()).getFirstName(), client.getFirstName());
+            Assert.assertEquals(clientDTOS.get(client.getId()).getEmail(), client.getEmail());
+            Assert.assertEquals(clientDTOS.get(client.getId()).getLastName(), client.getLastName());
+        }
+
+    }
+
+    //find all clients where name is equals "Masha" and email equals "masha@mail.com"
+    @Test
+    public void find2_Test_OK() throws Exception {
+        String searchRequest ="[{\n" +
+                "    \"field\": \"firstName\",\n" +
+                "    \"operation\": \"EQUALITY\",\n" +
+                "    \"orPredicate\": true,\n" +
+                "    \"value\": \"Masha\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"field\": \"email\",\n" +
+                "    \"operation\": \"EQUALITY\",\n" +
+                "    \"orPredicate\": false,\n" +
+                "    \"value\": \"masha@mail.com\"\n" +
+                "  }]";
+        RequestPostProcessor bearerToken = addBearerToken("test");
+
+        MvcResult result = mockMvc.perform(post("/client-journal-resource/clients/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(searchRequest)
+                .with(bearerToken))
+                .andExpect(status().is(200)).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+
+        List<ClientDTO> clients = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<ClientDTO>>() {
+        });
+        Assert.assertEquals(1, clients.size());
+        for (ClientDTO client : clients) {
+            Assert.assertEquals(clientDTOS.get(client.getId()).getFirstName(), client.getFirstName());
+            Assert.assertEquals(clientDTOS.get(client.getId()).getEmail(), client.getEmail());
+            Assert.assertEquals(clientDTOS.get(client.getId()).getLastName(), client.getLastName());
+        }
+    }
+
+    //find all clients where name is equals "Masha" and email equals "petya@mail.com"
+    @Test
+    public void find3_Test_OK() throws Exception {
+        String searchRequest ="[{\n" +
+                "    \"field\": \"firstName\",\n" +
+                "    \"operation\": \"EQUALITY\",\n" +
+                "    \"orPredicate\": true,\n" +
+                "    \"value\": \"Masha\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"field\": \"email\",\n" +
+                "    \"operation\": \"EQUALITY\",\n" +
+                "    \"orPredicate\": false,\n" +
+                "    \"value\": \"petya@mail.com\"\n" +
+                "  }]";
+        RequestPostProcessor bearerToken = addBearerToken("test");
+
+        MvcResult result = mockMvc.perform(post("/client-journal-resource/clients/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(searchRequest)
+                .with(bearerToken))
+                .andExpect(status().is(200)).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+
+        List<ClientDTO> clients = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<ClientDTO>>() {
+        });
+        Assert.assertEquals(0, clients.size());
+    }
+
+    //find all clients where name starts with ""m
+    @Test
+    public void find4_Test_OK() throws Exception {
+        String searchRequest ="[{\n" +
+                "    \"field\": \"firstName\",\n" +
+                "    \"operation\": \"LIKE\",\n" +
+                "    \"orPredicate\": true,\n" +
+                "    \"value\": \"M%\"\n" +
+                "  }]";
+        RequestPostProcessor bearerToken = addBearerToken("test");
+
+        MvcResult result = mockMvc.perform(post("/client-journal-resource/clients/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(searchRequest)
+                .with(bearerToken))
+                .andExpect(status().is(200)).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+
+        List<ClientDTO> clients = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<ClientDTO>>() {
+        });
+        Assert.assertEquals(2, clients.size());
+        for (ClientDTO client : clients) {
+            Assert.assertEquals(clientDTOS.get(client.getId()).getFirstName(), client.getFirstName());
+            Assert.assertEquals(clientDTOS.get(client.getId()).getEmail(), client.getEmail());
+            Assert.assertEquals(clientDTOS.get(client.getId()).getLastName(), client.getLastName());
+        }
+    }
+
+}
